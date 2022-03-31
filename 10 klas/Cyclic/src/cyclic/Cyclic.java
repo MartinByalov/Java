801a60ff08983ec4d098b129db7dec4dc0cd8e8f
MartinByalov <35262068+MartinByalov@users.noreply.github.com>
2022-03-31 22:54:36 +0300

    Add files via upload

---
+++ 10 klas/Cyclic/src/cyclic/Cyclic.java
@@ -0,0 +1,26 @@
+package cyclic;
+
+import java.util.Scanner;
+
+// Пресметни i
+public class Cyclic {
+
+	// Начало
+	public static void main(String[] args) {
+
+		// Четене на входни данни от потребителя
+		Scanner input = new Scanner(System.in);
+		
+		/*
+		 * За i = 1
+		 * Ако i <= 100
+		 * Увеличавай i с 1
+		 */
+		for(int i = 1; i <= 100; i++) {
+			
+			// Стъпка 1: Изведи i
+			System.out.println("i = " + i);
+		}
+	}
+	// Край
+}
